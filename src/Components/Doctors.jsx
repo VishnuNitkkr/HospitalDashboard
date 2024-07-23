d9a087f6866790e5bc1aabdeb3b96947f51f33b3
VishnuNitkkr <vk9509910539@gmail.com>
2024-07-23 12:03:51 +0530

    first commit

---
+++ src/Components/Doctors.jsx
@@ -0,0 +1,81 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../main";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Navigate } from "react-router-dom";
+
+const Doctors = () => {
+  const [doctors, setDoctors] = useState([]);
+  const { isAuthenticated } = useContext(Context);
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://hospital-management-backend-6v1yozd41-jadhavmanoj2023s-projects.vercel.app/api/v1/user/doctors",
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setDoctors(data.doctors);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    };
+    fetchDoctors();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+
+  return (
+    <>
+      <section className="page doctors">
+        <h1>DOCTORS</h1>
+        <div className="banner">
+          {doctors && doctors.length > 0 ? (
+            doctors.map((element) => {
+              return (
+                <div className="card">
+                  <img
+                    src={element.docAvatar && element.docAvatar.url}
+                    alt="Doctor Avatar"
+                  />
+                  <h4>{`${element.firstName} ${element.lastName}`}</h4>
+                  <div className="details">
+                    <p>
+                      Email : <span>{element.email}</span>
+                    </p>
+                    <p>
+                      Phone : <span>{element.phone}</span>
+                    </p>
+                    <p>
+                      DOB : <span>{element.dateOfBirth.substring(0, 10)}</span>
+                    </p>
+                    <p>
+                      Department : <span>{element.doctorDepartment}</span>
+                    </p>
+                    <p>
+                      aadhar : <span>{element.aadhar}</span>
+                    </p>
+                    <p>
+                      Gender : <span>{element.gender}</span>
+                    </p>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <h1> NO registered doctors found</h1>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Doctors;
